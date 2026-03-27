33829e4be38f872d9be8233292ef495bbac6c15a
Johan Sánchez Vásquez <johannsava09@gmail.com>
2026-03-26 18:43:14 -0600

    Update

--- src/main/java/facturacion/proyecto/util/ConexionBD.java
+++ src/main/java/facturacion/proyecto/util/ConexionBD.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class ConexionBD {
 
     private static final String USER = "PROYECTO_FACTURA_ELECTRONICA";
-    private static final String PASSWORD = "Anjosava2026.@";
+    private static final String PASSWORD = "Fidelitas_2026+";
     private static final String URL = "jdbc:oracle:thin:@lenguajesbd_low";
     private static final String TNS_ADMIN = "J:/LenguajesBD/wallet/Wallet_LenguajesBD";
 
